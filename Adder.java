8773439cde3bdcb89663e94322f740612c8cba6a
ok <60050196@kmitl.ac.th>
2018-09-10 18:47:29 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Adder {
+    public static void main(String[] args) {
+        System.out.println("Hello CS");
+        Scanner Sc = new Scanner(System.in);
+        int x; int y; int sum;
+        System.out.println("X=");
+        x = Sc.nextInt();
+        System.out.println("Y=");
+        y = Sc.nextInt();
+        sum = x+y;
+        System.out.println("Sum="+sum);
+
+    }
+
+}
+    
